dd466d9d3a91fb82f2b77ab4f3fe7c966b74ab17
Willian Prado <willian_prado@hotmail.com>
2022-03-15 15:28:54 -0300

    feat(search_engine): add `search_by_date` function

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,16 +1,30 @@
 from tech_news.database import search_news
+from datetime import datetime, timedelta
 
 
 # Requisito 6
 def search_by_title(title):
-    news = search_news({"title": {'$regex': title, '$options': 'i'}})
-    title_tuple = [(report['title'], report['url']) for report in news]
+    news = search_news({"title": {"$regex": title, "$options": "i"}})
+    title_tuple = [(report["title"], report["url"]) for report in news]
     return title_tuple
 
 
 # Requisito 7
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    try:
+        format_date = datetime.strptime(date, "%Y-%m-%d")
+    except ValueError:
+        raise ValueError("Data inválida")
+
+    news = search_news({
+        "timestamp": {
+            "$lt": datetime.isoformat(format_date + timedelta(days=1)),
+            "$gte": datetime.isoformat(format_date)
+        }
+    })
+
+    title_tuple = [(report["title"], report["url"]) for report in news]
+    return title_tuple
 
 
 # Requisito 8
